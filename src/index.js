9a601d2577c0b6c854fd749932dad45f3e56bfb6
Zephyr Wong <zephyrwong@oursky.com>
2018-02-11 19:45:36 +0800

    Use different endpoints in dev and prod env

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
+let endPoint = "https://reactdinnerpoll.skygeario.com/";
+let apiKey = "8b05aba14a3e4b53a93277ae93a53c13";
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  endPoint = "https://zephyrdev.skygeario.com/";
+  apiKey = "0a09dd866b8f40df88a48147f4ac4e57";
+}
+
 skygear
   .config({
-    endPoint: "https://reactdinnerpoll.skygeario.com/",
-    apiKey: "8b05aba14a3e4b53a93277ae93a53c13"
+    endPoint,
+    apiKey
   })
-  .catch(error => {
-    console.error(error);
-  });
+  .catch(error => console.error(error));
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
